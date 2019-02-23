80be7a401453f53e122e1129218dc2ed1d5bc6bb
jamesandere <jamesandere56@gmail.com>
2019-02-24 00:47:38 +0300

    Use control flows

--- js/scripts.js
+++ js/scripts.js
@@ -46,3 +46,32 @@ $("button#but1").click(function marks(){
       var total = (quesOne + quesTwo + quesThree + quesFour + quesFive);
 
       var percentage = (total/25)*100;
+
+      if(percentage >= 80 ){
+            document.getElementById("results").innerHTML="You got " + percentage + "%";
+            document.getElementById("outcome").innerHTML="Excellent job! You are ready to be a coder!"
+            document.getElementById("img").src="https://media1.tenor.com/images/f676ac7683d9d3c545ddea12ec05b60b/tenor.gif?itemid=4115420"
+          }
+          else if(percentage >= 50){
+            document.getElementById("results").innerHTML="You got " + percentage + "%";
+            document.getElementById("outcome").innerHTML="That was a great attempt."
+            document.getElementById("img").src="https://media.tenor.com/images/937a27fddf2716840fce110c6e0a1414/tenor.gif"
+          }
+          else{
+            document.getElementById("results").innerHTML="You got " + percentage + "%";
+            document.getElementById("outcome").innerHTML="That was a poor attempt. You need to retake the test."
+            document.getElementById("img").src="https://media.tenor.com/images/1a4f871c86ba1383170b2e187edb98a5/tenor.gif"
+          }
+
+      })
+      $("#but6").click(function(){
+        $("#results").hide()
+        $("#outcome").hide()
+        $("#but6").hide()
+        $("#img").hide()
+        $("#jav").hide()
+        $("#vaj").hide()
+        $(".ras").show();
+      })
+
+      });
